dccb3ad13fcab916470122613c985fe137b20a4e
Iulia Constantin <33063360+iulia2799@users.noreply.github.com>
2023-04-30 15:59:29 +0300

    make some changes

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SignalrService } from 'src/app/services/signalr.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
 
+
+  constructor(
+    private signalRService: SignalrService
+  ){
+    this.signalRService = new SignalrService();
+  }
+
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import * as signalR from '@microsoft/signalr';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignalrService {
 
-  hubUrl?:string;
-  connection: any;
-  
-  constructor() { }
+  private hubConnection!: HubConnection;
+  constructor() { 
+    this.hubConnection = new HubConnectionBuilder().withUrl('localhost:4200').build();
+    this.hubConnection.start()
+    .then(() => console.log('Connected to SignalR hub'))
+    .catch(err => console.error(err));
+  }
+
+  subscribeConnection(){
+    this.hubConnection.on('message-sent',(data: any) => {
+      console.log(`Received data: ${data}`);
+    })
+  }
+
+  invokeConnection(data: any) {
+    this.hubConnection.invoke('message-sent', data)
+    .catch(err => console.error(err));
+  }
+
 }
