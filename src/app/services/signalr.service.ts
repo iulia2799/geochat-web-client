e015a6ea20cb4997e4baa1f568b8ac62d0be6bd9
David <merodavid30@yahoo.com>
2023-05-22 21:52:23 +0300

    add signalR event handlers

--- src/app/services/signalr.service.ts
+++ src/app/services/signalr.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { ChatReadDto, MessageReadDto } from '../chatApi/models';
 
 @Injectable({
   providedIn: 'root'
@@ -8,21 +9,29 @@ export class SignalrService {
 
   private hubConnection!: HubConnection;
   constructor() { 
-    this.hubConnection = new HubConnectionBuilder().withUrl('localhost:4200').build();
+    var accessToken: string = localStorage.getItem("token") ?? "";
+    this.hubConnection = new HubConnectionBuilder().withUrl('https://geochatdefaultchat.azurewebsites.net/chatHub', 
+    {
+        accessTokenFactory: () => accessToken 
+    }).build();
     this.hubConnection.start()
     .then(() => console.log('Connected to SignalR hub'))
     .catch(err => console.error(err));
   }
 
-  subscribeConnection(){
-    this.hubConnection.on('message-sent',(data: any) => {
-      console.log(`Received data: ${data}`);
+  subscribeConnection(chatCreatedCallback: Function, messageReceivedCallback: Function){
+    this.hubConnection.on('MessageReceived',(data: MessageReadDto) => {
+      console.log(`Received message data: ${data}`);
+      chatCreatedCallback(data);
+    })
+
+    this.hubConnection.on('ChatCreated',(data: ChatReadDto) => {
+        console.log(`Received chat data: ${data}`);
+        messageReceivedCallback(data);
+      })
+
+    this.hubConnection.onclose((e) => {
+        console.error(e);
     })
   }
-
-  invokeConnection(data: any) {
-    this.hubConnection.invoke('message-sent', data)
-    .catch(err => console.error(err));
-  }
-
 }
