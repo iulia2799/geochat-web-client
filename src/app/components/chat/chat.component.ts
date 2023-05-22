48f9a770aa79488422777817937f3e45dce3a559
Iulia Constantin <33063360+iulia2799@users.noreply.github.com>
2023-05-22 10:42:25 +0300

    fix

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,24 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { SignalrService } from 'src/app/services/signalr.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
+
 export class ChatComponent implements OnInit {
 
 
   constructor(
-    private signalRService: SignalrService,
-    private http: HttpClient
+    private signalRService: SignalrService
   ){
     this.signalRService = new SignalrService();
   }
-  
+
 
   ngOnInit(): void {
+
   }
 }
